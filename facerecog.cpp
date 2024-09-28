a943eabb26574db6169b975bc81b1f04dad2d9d1
janeees <165977563+janeees@users.noreply.github.com>
2024-09-28 21:51:59 +0700

    Add files via upload

---
+++ facerecog.cpp
@@ -0,0 +1,45 @@
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+#include<opencv2/imgproc.hpp>
+#include<opencv2/objdetect.hpp>
+#include<iostream>
+
+
+using namespace std;
+using namespace cv;
+
+void main() {
+
+	VideoCapture video(1); //webcam
+	CascadeClassifier facedetect;
+	Mat img; //Mat = matrix variable
+	facedetect.load("haarcascade_frontalface_default.xml"); 
+
+	
+
+	while (true)
+	{
+		video.read(img);
+
+		vector<Rect> faces;
+
+		facedetect.detectMultiScale(img, faces, 1.3, 5);
+
+		cout << faces.size() << endl;
+
+		for (int i = 0; i < faces.size(); i++) { //untuk menghitung berapa wajah yang ada diframe kamera
+			rectangle(img, faces[i].tl(), faces[i].br(), Scalar(50, 50, 255), 3);
+			rectangle(img, Point(0, 0), Point(250, 70), Scalar(50, 50, 255), FILLED); //function rectangle ini untuk menandai wajah yang ditangkap kamera dan si AI
+			putText(img, to_string(faces.size()) + " Face Founded", Point(10, 40), FONT_HERSHEY_DUPLEX, 1, Scalar(255, 255, 255), 1);
+		}
+
+
+		imshow("Frame", img);
+		waitKey(1);
+
+
+	}
+
+}
+
+
